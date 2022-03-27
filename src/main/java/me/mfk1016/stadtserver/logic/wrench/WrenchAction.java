f049aca3b8e434667a0331de3a5383a4e792f214
Kilian Kotelewsky <kilian4991@gmail.com>
2022-03-27 22:09:29 +0200

    Add shulker box sorting.

--- src/main/java/me/mfk1016/stadtserver/logic/wrench/WrenchAction.java
+++ src/main/java/me/mfk1016/stadtserver/logic/wrench/WrenchAction.java
@@ -20,6 +20,11 @@ public abstract class WrenchAction {
             case NOTE_BLOCK -> new WrenchActionNoteBlock();
             case CHEST -> new WrenchActionInventory("Chest");
             case BARREL -> new WrenchActionInventory("Barrel");
+            case SHULKER_BOX, BLACK_SHULKER_BOX, BLUE_SHULKER_BOX,
+                    BROWN_SHULKER_BOX, CYAN_SHULKER_BOX, GRAY_SHULKER_BOX, GREEN_SHULKER_BOX,
+                    LIGHT_BLUE_SHULKER_BOX, MAGENTA_SHULKER_BOX, LIGHT_GRAY_SHULKER_BOX, LIME_SHULKER_BOX,
+                    ORANGE_SHULKER_BOX, PINK_SHULKER_BOX, PURPLE_SHULKER_BOX, RED_SHULKER_BOX, WHITE_SHULKER_BOX,
+                    YELLOW_SHULKER_BOX -> new WrenchActionInventory("Shulker Box");
             case BEE_NEST, BEEHIVE -> new WrenchActionBeeCheck();
             default -> new WrenchActionNothing();
         };
